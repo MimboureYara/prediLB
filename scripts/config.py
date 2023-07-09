288f362e523f78be59ec3c8a64c730118871fd3a
MimboureYARA <dmsy2014@gmail.com>
2023-07-09 16:06:05 +0000

    première mofication du fichier config.py

--- scripts/config.py
+++ scripts/config.py
@@ -8,4 +8,4 @@ class Config:
     DATASET_PATH = ASSETS_PATH / "data" # Dossier pour notre dataset
     FEATURES_PATH = ASSETS_PATH / "features" # Dossier pour les features 
     MODELS_PATH = ASSETS_PATH / "models" # Dossier pour les modeles
-    #METRICS_FILE_PATH = ASSETS_PATH / "metrics.json" # Fichiers pour les mesures
+    METRICS_FILE_PATH = ASSETS_PATH / "metrics.json" # Fichiers pour les mesures
